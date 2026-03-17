97f445f5a2c18d799f51b89807d345e220396084
Roman <romaferrero@hotmail.com>
2026-03-17 18:01:26 -0300

    Implemented daisyUI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
-import './App.css'
+import Button from './components/Button'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div>app</div>
+      <div>
+        <Button></Button>
+      </div>
   )
 }
 
